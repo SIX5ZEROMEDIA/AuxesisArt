10be55b44bad2658efa438c98dfb30982e10a654
Aaron Lambrecht <alambrecht115@gmail.com>
2021-02-28 12:28:29 -0800

    Adding changes to index.html and style.css homepage.

--- mobil.js
+++
@@ -1,9 +0,0 @@
-// Mobile Menu Toggle Button JavaScript
-function myFunction() {
-    var x = document.getElementById("mobile-menu");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
-  }
